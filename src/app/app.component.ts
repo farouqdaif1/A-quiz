2b7cd28a08522991af5ef6ec6ca1afe215da5358
farouqdaif1 <daiffarouq@gmail.com>
2024-09-05 03:18:56 +0300

    Intstall packages needed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { CardComponent } from './card/card.component';
+import { TableComponent } from './table/table.component';
+import { ChartComponent } from './chart/chart.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<h1>{{title}}</h1>`,
+  imports: [CardComponent, TableComponent, ChartComponent],
+  template: `
+    <div class="container">
+    <app-chart></app-chart>
+
+      <app-card></app-card>
+      <app-table></app-table>
+</div>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
